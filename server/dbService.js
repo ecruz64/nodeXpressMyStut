da659ef45fa89b2136ec43623d48af79bf79b990
ecruz <ecruz64@me.com>
2021-11-26 16:24:46 -0500

    App is running

---
+++ server/dbService.js
@@ -0,0 +1,22 @@
+const mysql=require('mysql');
+const dotenv=require('dotenv');
+const {NULL} = require("mysql/lib/protocol/constants/types");
+let instance = null;
+dotenv.config();
+
+const connection=mysql.createConnection({
+    host:       process.env.HOST,
+    user:       process.env.USER,
+    password:   process.env.PASSWORD,
+    database:   process.env.DATABASE,
+    port:       process.env.DB_PORT
+});
+
+connection.connect((err)=>{
+    if (err){
+        console.log(err.message);
+    }
+    console.log('db '+ connection.state);
+});
+
+module.exports=DbService;
