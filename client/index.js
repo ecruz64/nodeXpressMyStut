b3ba5f3952a5a0bc5d2bd1a23661e0a6b69ee636
ecruz <ecruz64@me.com>
2021-11-26 16:09:36 -0500

    getting an error

--- client/index.js
+++ client/index.js
@@ -1,11 +1,13 @@
 document.addEventListener('DOMContentLoaded', function (){
-    loadHTMLTable([]);
+fetch('https://localhost:443/getAll')
+    .then(response=> response.json())
+    .then(data=>loadHTMLTable(data['data']));
 });
 
 
 function loadHTMLTable(data){
     const table = document.querySelector('table tbody');
-
+    console.log(data);
     if (data.length === 0){
         table.innerHTML = "<tr><td class='no-data'  colspan='5'>No Data</td></tr>";
     }
